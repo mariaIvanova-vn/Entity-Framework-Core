db0ba464c951add559f907ab3fff999b0a9eda2e
mariaIvanova-vn <maria_vn86@abv.bg>
2023-02-27 15:09:18 +0200

    add

---
+++ 01.Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,70 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetEmployeesWithSalaryOver50000(dbContext);
+            Console.WriteLine(result);
+        }
+
+
+        //3.	Employees Full Information
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var allEmployees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+
+                })
+                .ToList();
+
+            foreach (var employee in allEmployees)
+            {
+                output.AppendLine(
+                    $"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+
+        //4.	Employees with Salary Over 50 000
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var highSalaryEmployees = context
+                .Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ToArray();
+
+            foreach (var e in highSalaryEmployees)
+            {
+                output.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}

cfcefa67aa210954bb5103343d5f8c3af24140c6
mariaIvanova-vn <maria_vn86@abv.bg>
2023-02-27 14:50:24 +0200

    add 02. Database First

---
+++ 01.Entity Framework Introduction/SoftUni/SoftUni/Program.cs
@@ -0,0 +1,13 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            Console.WriteLine("Connect!");
+        }
+    }
+}

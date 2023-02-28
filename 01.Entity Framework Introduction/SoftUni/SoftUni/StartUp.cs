d8984a5ce07ef7f155e979015e5760d9563cf571
mariaIvanova-vn <maria_vn86@abv.bg>
2023-02-28 09:51:07 +0200

    add

--- 01.Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
+++ 01.Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using SoftUni.Data;
+using SoftUni.Models;
 using System.Text;
 
 namespace SoftUni
@@ -9,7 +10,7 @@ namespace SoftUni
         {
             SoftUniContext dbContext = new SoftUniContext();
 
-            string result = GetEmployeesWithSalaryOver50000(dbContext);
+            string result = AddNewAddressToEmployee(dbContext);
             Console.WriteLine(result);
         }
 
@@ -65,6 +66,49 @@ namespace SoftUni
             }
 
             return output.ToString().TrimEnd();
+        }
+
+
+        //5.	Employees from Research and Development
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var emploeeRND = context.Employees
+               .Where(e => e.Department.Name == "Research and Development").Select(
+                e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    departmentName = e.Department.Name,
+                    e.Salary
+                }).OrderBy(e => e.Salary).ThenByDescending(e => e.FirstName).ToArray();
+
+            foreach (var item in emploeeRND)
+            {
+                output.AppendLine($"{item.FirstName} {item.LastName} from {item.departmentName} - ${item.Salary:f2}");
+            }
+            return output.ToString().TrimEnd();
+        }
+
+        //6.	Adding a New Address and Updating Employee
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            Address newAddress = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            Employee employee = context.Employees.FirstOrDefault(e => e.LastName == "Nakov");
+            employee.Address=newAddress;
+
+            context.SaveChanges();
+
+            var emploeeAddress = context.Employees.OrderByDescending(e => e.AddressId).Take(10)
+                .Select(e => e.Address.AddressText).ToArray();
+
+            return String.Join(Environment.NewLine, emploeeAddress);
         }
     }
 }

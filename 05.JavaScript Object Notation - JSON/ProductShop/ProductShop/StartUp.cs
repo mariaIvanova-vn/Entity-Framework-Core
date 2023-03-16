ec8980c3a9e1ed869cf3978523d586872be17b19
mariaIvanova-vn <maria_vn86@abv.bg>
2023-03-16 14:40:34 +0200

    add

--- 05.JavaScript Object Notation - JSON/ProductShop/ProductShop/StartUp.cs
+++ 05.JavaScript Object Notation - JSON/ProductShop/ProductShop/StartUp.cs
@@ -16,7 +16,7 @@ namespace ProductShop
             var context = new ProductShopContext();
             //string inputJson = File.ReadAllText(@"../../../Datasets/categories-products.json");
 
-            string result = GetCategoriesByProductsCount(context);
+            string result = GetUsersWithProducts(context);
             Console.WriteLine(result);
         }
 
@@ -179,6 +179,42 @@ namespace ProductShop
                 }).AsNoTracking().ToArray();
 
                  return JsonConvert.SerializeObject(result, Formatting.Indented);
+        }
+
+
+        //Query 8. Export Users and Products
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(u=>u.ProductsSold.Any(p=>p.Buyer != null))
+                .Select(u => new
+                {
+                    firstName = u.FirstName,
+                    lastName = u.LastName,
+                    age= u.Age,
+                    soldProducts = new
+                    {
+                        count = u.ProductsSold.Count(p=>p.Buyer!=null),
+                        products = u.ProductsSold.Where(p=>p.Buyer!=null)
+                        .Select(p => new
+                        {
+                            name=p.Name,
+                            price= p.Price
+                        }).ToArray()
+                    }
+                }).OrderByDescending(u=>u.soldProducts.count)
+                .AsNoTracking().ToArray();
+
+            var result = new
+            {
+                usersCount = users.Length,
+                users = users
+            };
+
+            return JsonConvert.SerializeObject(result, Formatting.Indented,new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+            });
         }
     }
 }

3f5ea56e7340f963f162846c48622166cf85495f
mariaIvanova-vn <maria_vn86@abv.bg>
2023-03-19 13:20:06 +0200

    add

--- 06.Extensible Markup Language - XML/CarDealer/CarDealer/StartUp.cs
+++ 06.Extensible Markup Language - XML/CarDealer/CarDealer/StartUp.cs
@@ -1,10 +1,59 @@
-﻿namespace CarDealer
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using CarDealer.Utilities;
+using System.Xml.Serialization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            CarDealerContext context = new CarDealerContext();
+            string inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
 
+            string result = ImportSuppliers(context, inputXml);
+            Console.WriteLine(result);
         }
+
+
+        //Query 9. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = InitializeAutoMapper();
+
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportSupplierDto[] supplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+
+            ICollection<Supplier> suppliers = new HashSet<Supplier>();
+            foreach (var item in supplierDtos)
+            {
+                if (string.IsNullOrEmpty(item.Name))
+                {
+                    continue;
+                }
+                //Manual mapping without automapping
+                //Supplier supplier = new Supplier()
+                //{
+                //    Name = item.Name,
+                //    IsImporter = item.IsImporter
+                //};
+
+                Supplier supplier = mapper.Map<Supplier>(item);
+                suppliers.Add(supplier);
+            }
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}";
+        }
+
+        private static IMapper InitializeAutoMapper() =>
+            new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
     }
 }
---
+++ 06.Extensible Markup Language - XML/CarDealer/CarDealer/Utilities/XmlHelper.cs
@@ -0,0 +1,24 @@
+﻿using CarDealer.DTOs.Import;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.Utilities
+{
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Suppliers");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            StringReader reader = new StringReader(inputXml);
+            T supplierDtos = (T)xmlSerializer.Deserialize(reader);
+
+            return supplierDtos;
+        }
+    }
+}

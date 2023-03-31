4eff04b6aea4094c274488642c66c9b1db98c027
mariaIvanova-vn <maria_vn86@abv.bg>
2023-03-31 10:59:15 +0300

    C# DB Advanced Exam - 06 August 2022

---
+++ EXAM PREPARATION/C# DB Advanced Exam - 06 August 2022/Footballers/Footballers/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Footballers.Data
+{
+    public class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-VDOVIB3\SQLEXPRESS04;Database=Footballers;Trusted_Connection=True";
+    }
+}
---
+++ EXAM PREPARATION/C# DB Advanced Exam - 06 August 2022/Footballers/Footballers/GlobalConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Footballers
+{
+    public class GlobalConstants
+    {
+        //Footballer
+
+    }
+}
